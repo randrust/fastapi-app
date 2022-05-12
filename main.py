3ad3bfe79acc626b3ffe846f0fdb70243f009e9e
Andrii Rakutskyi <andru.rakutsky@gmail.com>
2022-05-12 20:51:18 +0300

    Add simple chameleon template

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 from unicodedata import name
 import fastapi
 import uvicorn
-
+import fastapi_chameleon
+from fastapi_chameleon import template
 
 app = fastapi.FastAPI()
 
+fastapi_chameleon.global_init('templates')
 
 @app.get('/')
-def index():
-    return {'message':'Hellow world!'}
-
+@template(template_file='index.html')
+def index(user: str = 'anon'):
+    return {
+        'user_name': user
+    }
 if __name__ == '__main__':
     uvicorn.run(app)
